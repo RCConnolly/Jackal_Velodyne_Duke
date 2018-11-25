7c5fa450ba28a68b8c89053595ae0d3468dfd90e
RCConnolly <ryan.connolly@duke.edu>
2018-11-25 00:52:01 -0500

    fixed whitespace and formatting

--- navigation/send_goal_client.py
+++ navigation/send_goal_client.py
@@ -7,12 +7,13 @@ import actionlib
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
-from geometry_msgs.msg import Pose, Point, Quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped
+# from geometry_msgs.msg import Pose, Point, Quaternion
 
 from sensor_msgs.msg import LaserScan
 
 from tf.transformations import quaternion_from_euler
+
 
 def getNearestObjectAngle():
     # returns the angle in sensor's frame to nearest object
@@ -32,7 +33,8 @@ def getNearestObjectAngle():
 
     return angle
 
-def createMoveBaseGoal(x,y,yaw,frame):
+
+def createMoveBaseGoal(x, y, yaw, frame):
     goal = MoveBaseGoal()
     goal.target_pose.pose.position.x = x
     goal.target_pose.pose.position.y = y
@@ -46,18 +48,18 @@ def createMoveBaseGoal(x,y,yaw,frame):
 
     goal.target_pose.header.frame_id = frame
 
-
     return goal
+
 
 def SendInitialPose(InitialPosePublisher, initial_pose):
     # goal: [x, y, yaw]
     InitialPoseMsg = PoseWithCovarianceStamped()
-    #InitialPoseMsg.header.seq = 0
+    # InitialPoseMsg.header.seq = 0
     InitialPoseMsg.header.stamp = rospy.Time.now()
     InitialPoseMsg.header.frame_id = 'map'
     InitialPoseMsg.pose.pose.position.x = initial_pose[0]
     InitialPoseMsg.pose.pose.position.y = initial_pose[1]
-    #InitialPoseMsg.pose.position.z = 0.0
+    # InitialPoseMsg.pose.position.z = 0.0
     quaternion = quaternion_from_euler(0, 0, initial_pose[2])
     InitialPoseMsg.pose.pose.orientation.x = quaternion[0]
     InitialPoseMsg.pose.pose.orientation.y = quaternion[1]
@@ -65,13 +67,16 @@ def SendInitialPose(InitialPosePublisher, initial_pose):
     InitialPoseMsg.pose.pose.orientation.w = quaternion[3]
     InitialPosePublisher.publish(InitialPoseMsg)
 
+
 def active_cb():
     rospy.loginfo("Goal pose is now being processed by the Move_Base Action Server...")
 
+
 def feedback_cb(feedback):
-    #To print current pose at each feedback:
-    #rospy.loginfo("Feedback for goal pose: "+str(feedback))
+    # To print current pose at each feedback:
+    # rospy.loginfo("Feedback for goal pose: "+str(feedback))
     return
+
 
 def done_cb(status, result):
     # Reference for terminal status values: http://docs.ros.org/diamondback/api/actionlib_msgs/html/msg/GoalStatus.html
@@ -88,7 +93,7 @@ def done_cb(status, result):
         else:
             rospy.loginfo("Final goal pose reached!")
             return
-            
+        
     if status == GoalStatus.ABORTED:
         rospy.loginfo("Goal pose  was aborted by the Action Server")
         rospy.signal_shutdown("Goal pose aborted, shutting down!")
@@ -106,8 +111,8 @@ def done_cb(status, result):
 def movebase_client(goal):
 
     # Create an action client called "move_base" with action definition file "MoveBaseAction"
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
- 
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+
     # Waits until the action server has started up and started listening for goals.
     client.wait_for_server()
 
@@ -124,24 +129,25 @@ def movebase_client(goal):
         rospy.signal_shutdown("Action server not available!")
     else:
         # Result of executing the action
-        return client.get_result()   
+        return client.get_result()
+
 
 # If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     try:
        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
         rospy.init_node('movebase_client_py')
-        initial = [0,0,0]
-        InitialPosePublisher = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size = 100)
+        initial = [0, 0, 0]
+        InitialPosePublisher = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=100)
         for i in range(10):
             SendInitialPose(InitialPosePublisher, initial)
             rospy.sleep(0.1)
-        result = movebase_client(createMoveBaseGoal(-4,-1,3.1, "map"))
+        result = movebase_client(createMoveBaseGoal(-4, -1, 3.1, "map"))
         if result:
             rospy.loginfo("Goal execution done!")
 
         closest_obj_angle = getNearestObjectAngle()
-        turn_goal = createMoveBaseGoal(0,0,closest_obj_angle, "front_laser")
+        turn_goal = createMoveBaseGoal(0, 0, closest_obj_angle, "front_laser")
         turn_res = movebase_client(turn_goal)
         if turn_res:
             rospy.loginfo("Successfully turned toward wall!")
